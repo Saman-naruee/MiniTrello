a6341a010b85c0cb89223402628e89a55cb89bcb
Saman-naruee <samannaruee@gmail.com>
2025-08-28 20:26:25 +0330

    set auth_user_model

--- MiniTrello/settings.py
+++ MiniTrello/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     # Local Apps
     'accounts',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -149,3 +149,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.User'
