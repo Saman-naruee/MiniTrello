fe28b82b46979dfa8184d488c5adb100a91ab64f
Saman-naruee <samannaruee@gmail.com>
2025-09-05 23:35:57 +0330

    add a test endpoint.

--- apps/playground/views.py
+++ apps/playground/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.models import User
+
 
 class TestPageView(ListView):
     template_name = 'test.html'
