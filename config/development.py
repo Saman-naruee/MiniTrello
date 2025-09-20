6ec987a344500da377d3750db25ec5fb78f7c09b
Saman-naruee <samannaruee@gmail.com>
2025-09-20 23:38:12 +0330

    update settings with postgres settings.

--- config/development.py
+++ config/development.py
@@ -7,12 +7,25 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Use SQLite for simple local development
-DATABASES = {
+SQLITE3 = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+POSTGRES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', cast=str),
+        'USER': config('DB_USER', cast=str),
+        'PASSWORD': config('DB_PASSWORD', cast=str),
+        'HOST': config('DB_HOST', cast=str),
+        'PORT': config('DB_PORT', cast=str),
+    }
+}
+
+DATABASES = POSTGRES
 
 # Use console backend for emails during development
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
