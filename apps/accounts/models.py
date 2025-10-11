348ce0dc6e6b107a9834bd5c5317dd13b79677d6
Saman-naruee <samannaruee@gmail.com>
2025-10-12 01:04:41 +0330

    Removed securety bug: allow new users to have staff and superuser permission!

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -24,8 +24,6 @@ class User(AbstractUser):
 
     # Django Auth Fields
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=True)
     is_email_verified = models.BooleanField(default=False)
     is_phone_verified = models.BooleanField(default=False)
 
